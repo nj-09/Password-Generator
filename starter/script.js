a2f39fd78d943d0831f58c3a85ce92ac414ef05b
Nishit <nishitjoshi.ichs@gmail.com>
2023-03-10 07:13:00 +0000

    latest changes]

--- starter/script.js
+++ starter/script.js
@@ -88,79 +88,87 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+var possibleCharacters = [];
+var passwordCharacters = [];
 
-// 
+
 function getPasswordOptions() {
   var upperCase = confirm("Would you like to have uppercase letters?");
   if (upperCase === true ) {
-    possibleCharacters.push(upperCasedCharacters);
+    possibleCharacters = possibleCharacters.concat(upperCasedCharacters);
     passwordCharacters.push(getRandom(upperCasedCharacters));
   }
   var lowerCase = confirm("Would you like to have lowercase letters?");
   if (lowerCase === true ) {
-    possibleCharacters.push(lowerCasedCharacters);
+    possibleCharacters = possibleCharacters.concat(lowerCasedCharacters);
     passwordCharacters.push(getRandom(lowerCasedCharacters));
   }
   var special = confirm("Would you like to have special characters?");
   if (special === true ) {
-    possibleCharacters.push(specialCharacters);
+    possibleCharacters = possibleCharacters.concat(specialCharacters);
     passwordCharacters.push(getRandom(specialCharacters));
   }
   var numeric = confirm("Would you like to have numbers?");
   if (numeric === true ) {
-    possibleCharacters.push(numericCharacters);
+    possibleCharacters = possibleCharacters.concat(numericCharacters);
     passwordCharacters.push(getRandom(numericCharacters));
   }
-  var length = prompt("How many characters would you like to have?")
+
+  var length = parseInt(prompt("How many characters would you like to have?"))
   while(length>128 || length <8){
     //repeat prompt
     alert("Password length must be between 8 and 128 characters, please try again.");
     length = prompt("How many characters would you like to have?");
   }
 
-// If statement to verify if all of your confirms are false, and if so then it would recall the function if none of the prompts are selected
-//if all the confirm are true, run this function
-if (confirm) {
-  // code that runs the next step of the generating password here}
-  // if any of them are false, go to else, and run function again
-} else {
-  getPasswordOptions()
-  }
-}
-
-// Function for getting a random element from an array
-function getRandom(arr) {
-  function getRandom(arr) {
-    let randomIndex = Math.floor(Math.random() * arr.length);
-    return arr[randomIndex];
-  }
-}
-
-// Function to generate password with user input
-// function generatePassword() {}
-// Loop method?
-
-// For loops and then output the password
+  // For loops and then output the password
 //// --------- For loop to take in passwordCharacters --------- ////
-// 1st option
-for (let i = 0; i < passwordCharacters.length; i++) {
-  text += passwordCharacters[i] + "<br>";
-}
-
-// 2nd option
-for (let i = 0; index < passwordCharacters.length; i++) {
-  const element = passwordCharacters[i];
+var password = [];
+for (let i = 0; i < array.length; i++) {
+  var character = getRandom[possibleCharacters];
   
-}
+  password.push(character);
+  }
 
 //// --------- For loop to take in  possibleCharacters --------- ////
-for (let i = 0; i < possibleCharacters.length; i++) {
-  text += possibleCharacters[i] + "<br>";
+for (let i = 0; i < array.length; i++) {
+password[i] = passwordCharacters[i];
+  
+  }
+
+console.log(password)
+
+// const finalPassword = ["length", "upperCase", "lowerCase", "specialCharacters"];
+let finalText = password.join();
+
+return finalText
+
+// Return the user's responses
+// return {
+  // length: length,
+  // upperCase: upperCase,
+ // lowerCase: lowerCase,
+ // specialCharacters: specialCharacters,
+//  numericCharacters: numericCharacters
+ // }
+
+        // Join the password without commas
+  // turns the array into a string
+  // return finalPassword let finalPassword = password.join('');
+
+ // return finalPassword
+
+// Return the user's responses
+
+
 }
+// Function for getting a random element from an array
+function getRandom(arr) {
+    let randomIndex = Math.floor(Math.random() * arr.length);
+    var randomElement = arr[randomIndex];
+    return randomElement;
 
-
-
-
+  }
 
 
 // Get references to the #generate element
@@ -172,6 +180,8 @@ function writePassword() {
   var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
+
+  
 }
 
 // Add event listener to generate button
@@ -179,3 +189,5 @@ generateBtn.addEventListener('click', writePassword);
 
 // Add event listener to getPasswordOptions
 generateBtn.addEventListener("click", getPasswordOptions);
+
+
